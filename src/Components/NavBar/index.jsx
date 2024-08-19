f14cfe89d10999ba01154fb0a96cf6ef57fb7af8
jayanta dey <jayantadey398@gmail.com>
2024-08-19 19:24:06 +0530

    home page responsive done.

--- src/Components/NavBar/index.jsx
+++ src/Components/NavBar/index.jsx
@@ -7,18 +7,55 @@ import { Box, Typography } from '@mui/material';
 
 //images
 import Clogo from "../../Assets/Images/Clogo.png";
-
-
+import MenuIcon from "../../Assets/Images/menuIcon.png"
+import backIcon from "../../Assets/Images/backIcon.png";
 //component
 
 
 export default function NavBar({ activeNav, setActiveNav }) {
   const navigate = useNavigate();
+  const [sideMenu, setSideMenu] = useState(false);
 
   return (
     <>
       <Box className="NavBar">
         <Box className="cLogoBox"><img src={Clogo} /></Box>
+        <img src={MenuIcon} className='menuIcon' onClick={() => setSideMenu(!sideMenu)} />
+
+
+        {/* side menu icon */}
+        <Box className="sideMenu" sx={{ width: sideMenu ? "100%" : "0px" }}>
+          <img src={backIcon} className='backIcon' onClick={() => setSideMenu(false)} />
+
+          <Box className={activeNav === 0 ? "navItem navItemActive" : "navItem"} onClick={() => {
+            setActiveNav(0)
+            navigate("/")
+          }}>
+            <Typography>Home</Typography>
+          </Box>
+          <Box className={activeNav === 1 ? "navItem navItemActive" : "navItem"} onClick={() => setActiveNav(1)}>
+            <Typography>Solutions</Typography>
+          </Box>
+          <Box className={activeNav === 2 ? "navItem navItemActive" : "navItem"} onClick={() => {
+            setActiveNav(2)
+            navigate("/about")
+
+          }}>
+            <Typography>About Us</Typography>
+          </Box>
+          <Box className={activeNav === 3 ? "navItem navItemActive" : "navItem"} onClick={() => setActiveNav(3)}>
+            <Typography>Blog</Typography>
+          </Box>
+          <Box className={activeNav === 4 ? "navItem navItemActive" : "navItem"} onClick={() => setActiveNav(4)}>
+            <Typography>Contact Us</Typography>
+          </Box>
+
+        </Box>
+
+
+
+
+
 
         <Box className="NavItemsBox">
           <Box className={activeNav === 0 ? "navItem navItemActive" : "navItem"} onClick={() => {
