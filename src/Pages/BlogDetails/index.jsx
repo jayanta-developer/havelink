3aafdc07947aea30162a99cb8b5d53b0a2fedb8b
jayanta dey <jayantadey398@gmail.com>
2024-09-19 18:58:07 +0530

    fix css

--- src/Pages/BlogDetails/index.jsx
+++ src/Pages/BlogDetails/index.jsx
@@ -119,7 +119,6 @@ export default function BlogDetail({ activeNav, setActiveNav }) {
           <Typography className="bdNText">Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.</Typography>
 
 
-
           <Box mb={4} className="blogCardBox">
             {currentData?.slice(-3).map((el, i) =>
               blogCard({
